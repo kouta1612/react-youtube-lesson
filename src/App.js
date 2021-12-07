5a8ca484b091421fc92a50792fe2cadcb96887af
kouta1612 <kouta1612world69@gmail.com>
2021-12-07 09:52:54 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tweet from './Tweet'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello React</h1>
+      <Tweet />
+    </div>
+  )
+}
+
+export default App
