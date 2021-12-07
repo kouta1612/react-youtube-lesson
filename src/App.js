88f153dc5f73c0b1b90adb0e91d1bc1771a67756
kouta1612 <kouta1612world69@gmail.com>
2021-12-07 21:51:17 +0900

    delete for another lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import Tweet from './Tweet'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
     </div>
   )
 }
